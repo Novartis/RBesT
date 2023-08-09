5a6f9fb82b311478f56ec2e10162adf4a76958bc
Sebastian Weber <sebastian.weber@novartis.com>
2023-08-09 13:19:48 +0200

    bump hashes

--- tools/make-ds.R
+++ tools/make-ds.R
@@ -43,7 +43,7 @@ make_internal_ds <- function() {
     calibration_meta["MD5"]  <- vals["MD5"]
 
     pkg_create_date  <- Sys.time()
-    pkg_sha <- "995e625"
+    pkg_sha <- "b67a2a7"
 
     if (gsub("\\$", "", pkg_sha) == "Format:%h") {
         pkg_sha <- system("git rev-parse --short HEAD", intern=TRUE)
