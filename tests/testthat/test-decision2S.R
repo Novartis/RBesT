55ba9040f478cdfe5190cdb39423c98f608d46dc
Daniel Sabanes Bove <daniel@rconis.com>
2026-01-12 11:39:43 +0800

    tested decision2S

---
+++ tests/testthat/test-decision2S.R
@@ -0,0 +1,39 @@
+## Mixed lower.tail usage also works
+## This is relevant to calculate the probability of "interim" outcomes.
+## Example:
+## P(theta1 - theta2 <= 0) > 0.05
+## P(theta1 - theta2 > theta_target) > 0.5
+
+decMixed <- decision2S(
+  qc = c(0, 0.8, 1.2),
+  pc = c(0.05, 0.5, 0.05),
+  lower.tail = c(TRUE, FALSE, TRUE)
+)
+
+test_that("print method works for mixed lower.tail", {
+  expect_snapshot(print(decMixed))
+})
+
+test_that("length method works", {
+  expect_equal(length(decMixed), 3L)
+})
+
+test_that("subsetting works", {
+  dec1 <- decMixed[1]
+  expect_equal(length(dec1), 1L)
+  expect_identical(attr(dec1, "lower.tail"), TRUE)
+  
+  dec2 <- decMixed[2]
+  expect_equal(length(dec2), 1L)
+  expect_identical(attr(dec2, "lower.tail"), FALSE)
+
+  dec12 <- decMixed[1:2]
+  expect_equal(length(dec12), 2L)
+  expect_identical(attr(dec12, "lower.tail"), c(TRUE, FALSE))
+
+  # This case is important: both criterions have same lower.tail,
+  # therefore it should be simplified to a single logical.
+  dec13 <- decMixed[c(1, 3)]
+  expect_equal(length(dec13), 2L)
+  expect_identical(attr(dec13, "lower.tail"), TRUE)
+})
