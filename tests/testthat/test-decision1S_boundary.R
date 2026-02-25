ec2cccf783f33b37df58d3924656a8044f94423d
Daniel Sabanes Bove <daniel@rconis.com>
2026-02-25 16:49:35 +0800

    adapt decision1S_boundary.gammaMix

--- tests/testthat/test-decision1S_boundary.R
+++ tests/testthat/test-decision1S_boundary.R
@@ -77,3 +77,43 @@ test_that("Mixed lower.tail usage works for binomial decision boundary calculati
   expected <- c(lower_or_equal_than = result_lower, higher_than = result_upper)
   expect_equal(result, expected)
 })
+
+test_that("decision1S_boundary works for Poisson outcome", {
+  prior <- mixgamma(rob = c(0.2, 2, 2), inf = c(0.8, 5, 5))
+
+  dec <- decision1S(pc = 0.95, qc = 0.6, lower.tail = TRUE)
+  result <- decision1S_boundary(prior, n = 50, decision = dec)
+  expect_equal(result, 19)
+
+  dec <- decision1S(pc = 0.6, qc = 0.6, lower.tail = FALSE)
+  result <- decision1S_boundary(prior, n = 50, decision = dec)
+  expect_equal(result, 30)
+})
+
+test_that("Mixed lower.tail usage works for Poisson decision boundary calculation", {
+  prior <- mixgamma(rob = c(0.2, 2, 2), inf = c(0.8, 5, 5))
+
+  dec_lower <- decision1S(pc = 0.5, qc = 0.7, lower.tail = TRUE)
+  result_lower <- decision1S_boundary(
+    prior,
+    n = 50,
+    decision = dec_lower
+  )
+
+  dec_upper <- decision1S(pc = 0.6, qc = 0.5, lower.tail = FALSE)
+  result_upper <- decision1S_boundary(
+    prior,
+    n = 50,
+    decision = dec_upper
+  )
+  assert_true(result_lower != result_upper)
+
+  decMixed <- decision1S(
+    qc = c(0.7, 0.5),
+    pc = c(0.5, 0.6),
+    lower.tail = c(TRUE, FALSE)
+  )
+  result <- decision1S_boundary(prior, n = 50, decision = decMixed)
+  expected <- c(lower_or_equal_than = result_lower, higher_than = result_upper)
+  expect_equal(result, expected)
+})
